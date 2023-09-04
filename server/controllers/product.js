2d147c09a4eb02fbf603c676b267e184218ffb64
PhucLeDio <dinhphucle3@gmail.com>
2023-09-04 15:59:52 +0700

    filter, sort, pagenation

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -15,7 +15,6 @@ const createProduct = asyncHandler(async (req, res) => {
 const getProduct = asyncHandler(async (req, res) => {
   const { pid } = req.params;
   const product = await Product.findById(pid);
-
   return res.status(200).json({
     success: product ? true : false,
     productData: product ? product : "Cannot get product",
@@ -24,11 +23,61 @@ const getProduct = asyncHandler(async (req, res) => {
 
 // filtering, sorting & pagination
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find();
-  return res.status(200).json({
-    success: products ? true : false,
-    productDatas: products ? products : "Cannot get products",
-  });
+  const queries = { ...req.query };
+  // tách các trường đặc biệt ra khỏi query
+  const excludeFields = ["limit", "sort", "page", "fields"];
+  excludeFields.forEach((el) => delete queries[el]);
+
+  // format lại cấu trúc operators cho giống mongoose
+  let queryString = JSON.stringify(queries);
+  queryString = queryString.replace(
+    /\b(gte|gt|lt|lte)\b/g,
+    (matchedEl) => `$${matchedEl}`
+  );
+  const formatedQueries = JSON.parse(queryString);
+
+  // filering
+  if (queries?.title)
+    formatedQueries.title = { $regex: queries.title, $options: "i" };
+  let queryCommand = Product.find(formatedQueries);
+
+  // sorting
+  // ex: abc,edg => [abc,edg] => abc edg
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    queryCommand = queryCommand.sort(sortBy);
+  }
+
+  // field limiting
+  if (req.query.fields) {
+    const fields = req.query.fields.split(",").join(" ");
+    queryCommand = queryCommand.select(fields);
+  }
+
+  // pagination (phân trang)
+  // giả sử lấy được 10 object 1 2 3 4 ... 10
+  // limit: số object lấy về 1 lần gọi API VD: limit = 2 => lấy 1 2
+  // skip: bỏ qua số object sau mỗi lần gọi VD: skip = 2 => lấy 3 4 ... 10
+  // limit = 2, skip = 2 => bỏ qua 1 2, lấy 3 4
+  // string +2 = int 2 :))
+  const page = +req.query.page || 1;
+  const limit = +req.query.limit || process.env.LIMIT_PRODUCTS;
+  const skip = (page - 1) * limit;
+  queryCommand.skip(skip).limit(limit);
+  // execute query
+  // số lượng sản phẩm thỏa mãn !=== số lượng sản phẩm trả về 1 lần gọi API (hàm exec ko call back đc nên => then | catch)
+  queryCommand
+    .then(async (response) => {
+      const counts = await Product.find(formatedQueries).countDocuments();
+      return res.status(200).json({
+        success: response ? true : false,
+        counts,
+        products: response ? response : "Cannot get products",
+      });
+    })
+    .catch((err) => {
+      throw new Error(err.message);
+    });
 });
 
 const updateProduct = asyncHandler(async (req, res) => {
@@ -52,10 +101,49 @@ const deleteProduct = asyncHandler(async (req, res) => {
   });
 });
 
+const ratings = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { star, comment, pid } = req.body;
+  if (!star || !pid) throw new Error("missing input");
+  const ratingProduct = await Product.findById(pid);
+  const alreadyRating = ratingProduct?.ratings?.find(
+    (el) => el.postedBy.toString() === _id
+  );
+  console.log(alreadyRating);
+  if (alreadyRating) {
+    // update star & comment
+    await Product.updateOne(
+      {
+        ratings: { $elemMatch: alreadyRating },
+      },
+      {
+        $set: { "ratings.$.star": star, "ratings.$.comment": comment },
+      },
+      { new: true }
+    );
+  } else {
+    // add star & comment
+    const response = await Product.findByIdAndUpdate(
+      pid,
+      {
+        $push: { ratings: { star, comment, postedBy: _id } },
+      },
+      { new: true }
+    );
+  }
+
+  // Sum ratings
+
+  return res.status(200).json({
+    status: true,
+  });
+});
+
 module.exports = {
   createProduct,
   getProduct,
   getProducts,
   updateProduct,
   deleteProduct,
+  ratings,
 };
