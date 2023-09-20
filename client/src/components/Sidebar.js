3884cccf60944b0edc2fcb707380be83beeda9ac
PhucLeDio <dinhphucle3@gmail.com>
2023-09-20 08:28:37 +0700

    base react

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sidebar = () => {
+  return <div>Slidebar</div>;
+};
+
+export default Sidebar;
---
+++ client/src/pages/public/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Sidebar, Banner } from "../../components";
+
+const Home = () => {
+  return (
+    <div className="w-main flex">
+      <div className="flex flex-col gap-5 w-[30%] flex-auto border">
+        <Sidebar />
+        <span>Deal daily</span>
+      </div>
+      <div className="flex flex-col gap-5 pl-5 w-[70%] flex-auto border">
+        <Banner />
+        <span>Best seller</span>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
