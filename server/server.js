adf5586156dc56feff95de441d97a137d6048b64
PhucLeDio <dinhphucle3@gmail.com>
2023-09-21 10:44:17 +0700

    Call API với redux toolkit server part

--- server/server.js
+++ server/server.js
@@ -3,8 +3,15 @@ require("dotenv").config();
 const dbconnect = require("./config/dbconnect");
 const initRoutes = require("./routes");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["POST", "PUT", "DELETE", "GET"],
+  })
+);
 app.use(cookieParser());
 const port = process.env.PORT || 8888;
 app.use(express.json());
