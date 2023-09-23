fd909aab8b66b9ebd9a78d4448dff08ea1bd2c87
PhucLeDio <dinhphucle3@gmail.com>
2023-09-23 22:04:39 +0700

    data

---
+++ data/cate_brand.js
@@ -0,0 +1,43 @@
+const data = [
+  {
+    cate: "Smartphone",
+    brand: ["Apple", "Samsung", "LG", "Asus", "Nokia", "BlackBerry"],
+  },
+  {
+    cate: "Tablet",
+    brand: ["iPad", "Samsung", "Acer", "Asus", "Lenovo", "Haier"],
+  },
+  {
+    cate: "Laptop",
+    brand: ["Asus", "Dell", "Acer", "Lenovo", "HP", "Macbook"],
+  },
+  {
+    cate: "Speaker",
+    brand: [],
+  },
+  {
+    cate: "Camera",
+    brand: [],
+  },
+  {
+    cate: "Accessories",
+    brand: [
+      "SmartsWatches",
+      "Headphone",
+      "Bluetooth",
+      "Keyboard",
+      "Mouse",
+      "Cases",
+    ],
+  },
+  {
+    cate: "Television",
+    brand: ["Apple", "Samsung", "LG", "Asus", "Nokia", "BlackBerry"],
+  },
+  {
+    cate: "Printer",
+    brand: ["Samsung", "Acer", "Asus", "Lenovo", "Haier"],
+  },
+];
+
+module.exports = data;
