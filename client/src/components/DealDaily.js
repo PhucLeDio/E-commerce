0bf6173fc124e0a9dfb851b613e70ce64ea56f28
PhucLeDio <dinhphucle3@gmail.com>
2023-09-26 13:30:53 +0700

    hot collection update

--- client/src/components/DealDaily.js
+++ client/src/components/DealDaily.js
@@ -100,7 +100,11 @@ const DealDaily = () => {
         />
         <span className="line-clamp-1 text-center">{dealdaily?.title}</span>
         <span className="flex">
-          {renderStarFromNumber(dealdaily?.totalRatings, 20)}
+          {renderStarFromNumber(dealdaily?.totalRatings, 20)?.map(
+            (el, index) => (
+              <span key={index}>{el}</span>
+            )
+          )}
         </span>
         <span>{`${formatMoney(dealdaily?.price)} VND`}</span>
       </div>
