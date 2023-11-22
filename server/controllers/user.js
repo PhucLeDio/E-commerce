35f3a207782a7a6508687f451dbb0c6a30a4dd0a
PhucLeDio <dinhphucle3@gmail.com>
2023-11-22 21:53:41 +0700

    server v55

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -125,7 +125,7 @@ const login = asyncHandler(async (req, res) => {
 
 const getCurrent = asyncHandler(async (req, res) => {
   const { _id } = req.user;
-  const user = await User.findById(_id).select("-refreshToken -password -role");
+  const user = await User.findById(_id).select("-refreshToken -password");
   return res.status(200).json({
     success: user ? true : false,
     rs: user ? user : "user not found",
