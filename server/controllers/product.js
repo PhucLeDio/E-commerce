e4299f2d1d1ddb107db3695b319c789e16cb479d
PhucLeDio <dinhphucle3@gmail.com>
2023-12-10 20:46:18 +0700

    on going

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -3,12 +3,18 @@ const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
 
 const createProduct = asyncHandler(async (req, res) => {
-  if (Object.keys(req.body).length === 0) throw new Error("Missing inputs");
-  if (req.body && req.body.title) req.body.slug = slugify(req.body.title);
+  const { title, price, description, brand, category, color } = req.body;
+  const thumb = req?.files.thumb[0]?.path;
+  const images = req.files?.images?.map((el) => el.path);
+  if (!(title && price && description && brand && category && color))
+    throw new Error("Missing inputs");
+  req.body.slug = slugify(title);
+  if (thumb) req.body.thumb = thumb;
+  if (images) req.body.images = images;
   const newProduct = await Product.create(req.body);
   return res.status(200).json({
     success: newProduct ? true : false,
-    createdProduct: newProduct ? newProduct : "Cannot create new product",
+    mes: newProduct ? "Created" : "Something went wrong",
   });
 });
 
