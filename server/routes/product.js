0e43bea907424cafed83f2f78950c76822ce72be
PhucLeDio <dinhphucle3@gmail.com>
2023-12-23 22:47:59 +0700

    v68 done

--- server/routes/product.js
+++ server/routes/product.js
@@ -24,7 +24,16 @@ router.put(
   ctrls.uploadImagesProduct
 );
 
-router.put("/:pid", [verifyAccessToken, isAdmin], ctrls.updateProduct);
+router.put(
+  "/:pid",
+  verifyAccessToken,
+  isAdmin,
+  uploader.fields([
+    { name: "images", maxCount: 10 },
+    { name: "thumb", maxCount: 1 },
+  ]),
+  ctrls.updateProduct
+);
 
 router.delete("/:pid", [verifyAccessToken, isAdmin], ctrls.deleteProduct);
 
