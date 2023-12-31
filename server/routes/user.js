d7fd22a83c89c31e2796945065acf3602937e26c
Truong Huynh My Tam <tamm150504@gmail.com>
2023-12-31 20:13:10 +0700

    manage user complete

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,6 +4,7 @@ const { verifyAccessToken, isAdmin } = require("../middlewares/verifyToken");
 const uploader = require("../config/cloudinary.config");
 
 router.post("/register", ctrls.register);
+router.post("/mock", ctrls.createUsers);
 router.put("/finalregister/:token", ctrls.finalRegister);
 router.post("/login", ctrls.login);
 router.get("/current", verifyAccessToken, ctrls.getCurrent);
@@ -12,7 +13,7 @@ router.get("/logout", ctrls.logout);
 router.post("/forgotpassword", ctrls.forgotPassword);
 router.put("/resetpassword", ctrls.resetPassword);
 router.get("/", [verifyAccessToken, isAdmin], ctrls.getUsers); // role là user thì cook nhé
-router.delete("/", [verifyAccessToken, isAdmin], ctrls.deleteUser); // role là user thì cook nhé
+router.delete("/:uid", [verifyAccessToken, isAdmin], ctrls.deleteUser); // role là user thì cook nhé
 router.put(
   "/current",
   verifyAccessToken,
---
+++ server/utils/constant.js
@@ -0,0 +1,52 @@
+exports.users = [
+  {
+    email: "user-01@gmail.com",
+    password: 123456,
+    firstname: "Tony 1",
+    lastname: "Jimmy",
+    mobile: "0123456789",
+    avatar: "Mytam1",
+  },
+  {
+    email: "user-02@gmail.com",
+    password: 123456,
+    firstname: "Tony 2",
+    lastname: "Jimmy",
+    mobile: "0123456729",
+    avatar: "Mytam2",
+  },
+  {
+    email: "user-03@gmail.com",
+    password: 123456,
+    firstname: "Tony 3",
+    lastname: "Jimmy",
+    mobile: "0123456739",
+    avatar: "Mytam3",
+  },
+  {
+    email: "user-04@gmail.com",
+    password: 123456,
+    firstname: "Tony 4",
+    lastname: "Jimmy",
+    mobile: "0123456749",
+    avatar: "Mytam4",
+  },
+  {
+    email: "user-05@gmail.com",
+    password: 123456,
+    firstname: "Tony 5",
+    lastname: "Jimmy",
+    mobile: "0123456759",
+    avatar: "Mytam5",
+  },
+];
+exports.roles = [
+  {
+    code: "admin",
+    value: "Admin",
+  },
+  {
+    code: "user",
+    value: "User",
+  },
+];
